27aa6b4eeb1df9729dbf827adc6a43d36805f02c
deusjeraldy <deusjeraldy@gmail.com>
2020-09-13 11:46:18 +0300

    readme

---
+++ src/widgets/Card/index.ts
@@ -0,0 +1,15 @@
+import Div from "../../dom/Div"
+import * as CSS from "../../css/index";
+
+export default ({ children, style }:
+     { children: Array<HTMLElement | Text>, 
+        style: CSS.Properties
+     }) => {
+    style = {
+        background: '#fff',
+        borderRadius: "2px",
+        boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+        ...style
+    }
+    return Div({ children, style })
+}
---
+++ types/layouts/BorderPane.d.ts
@@ -0,0 +1,8 @@
+declare const _default: ({ top, left, right, bottom, center }: {
+    top?: any;
+    bottom?: any;
+    left?: any;
+    right?: any;
+    center: any;
+}) => HTMLElement;
+export default _default;
