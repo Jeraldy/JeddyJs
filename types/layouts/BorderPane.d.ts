2bbee4c9f51b3509e677f7704e3705c839483e8c
deusjeraldy <deusjeraldy@gmail.com>
2020-09-17 09:48:26 +0300

    readme

--- types/layouts/BorderPane.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: ({ top, left, right, bottom, center }: {
-    top?: any;
-    bottom?: any;
-    left?: any;
-    right?: any;
-    center: any;
-}) => HTMLElement;
-export default _default;
