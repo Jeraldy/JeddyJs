1c5d3e08f0d1db664bd4f0c72eec5ea23c39f8cc
deusjeraldy <deusjeraldy@gmail.com>
2021-05-26 11:26:56 +0300

    jeddy-scripts

--- jeddy-scripts/index.js
+++ jeddy-scripts/index.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
-console.log("WORKS...")
+var args = process.argv.splice(2)[0]
+const fse = require('fs-extra');
+var exec = require('child_process').exec;
+
+switch (args) {
+    case "start":
+        const srcDir = `node_modules/jeddy-types`;
+        const destDir = `node_modules/@types/jeddy`;
+        fse.copySync(srcDir, destDir, { overwrite: true }, (err) => console.error(err));
+        startServer()
+        break;
+    case "build":
+        console.log("************BUILDING STARTED************")
+        buildScript()
+        fse.copySync("public", "build", { overwrite: true }, (err) => console.error(err));
+        break;
+    default:
+        break
+}
+
+function startServer() {
+    var cmd = exec('webpack-dev-server --open --config webpack.dev.js');
+    cmd.stdout.on('data', (data) => console.log(data));
+}
+
+function buildScript() {
+    var cmd = exec('webpack --config webpack.prod.js');
+    cmd.stdout.on('data', (data) => console.log(data));
+}
